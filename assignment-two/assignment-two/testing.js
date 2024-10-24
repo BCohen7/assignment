6cc24f6e5e62f3b5dcaebce56065ecea8947f559
BCohen7 <bcoh0002@student.monash.edu>
2024-10-25 00:18:46 +1100

    Woo!

--- assignment-two/assignment-two/testing.js
+++ assignment-two/assignment-two/testing.js
@@ -1,16 +1,18 @@
 // Create a variable for the filter value to be stored within
-var XHold
+var xFunction;
 
-var pullOne = "symbolmap.json" // Import the map to be sent to figure
+var pullOne = "symbolmap.json"; // Import the map to be sent to figure
 
 // Once the map has been inserted to the HTML, we can pull its view to inspect for any X filter
 vegaEmbed('#vis1', pullOne).then(function(result) {
     // Access the Vega View instance
-    var view = result.view
+    var view = result.view;
 
     // Pull the X_Select value
-    var XPull = view.data('X_Select')
-    XHold(XPull)
+    view.addEventListener('click', function(event, item) {
+        var selectedRange = item.datum.XSelect;
+        xFunction(selectedRange);
+    });
 }).catch(console.error);
 
 // Now the bar chart can be imported, sent to figure, but checked for the filter on the way
@@ -21,7 +23,7 @@ vegaEmbed('#vis2', pullTwo).then(function(result) {
     var view = result.view
 
     // Push the state that is caught on click into the value to filter the chart
-    XHold = function SendX(newX) {
-        view.signal('XSel', newX).run();
+    xHold = function updateParameter(newValue) {
+        view.signal('XSelect', newValue).run();
     }
 })
